51d58c09c7010f826b50809b10447913c93866e2
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-04-25 13:13:40 -0500

    refactored slightly. Untested

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import json
 import os
-import base64
 import urllib.request
+base_url = "https://graph.facebook.com/v2.3"
 
-url = "https://graph.facebook.com/v2.3/me/photos"
 #super secret api password thingy!
 facebook_api_key = os.getenv('FACEBOOK_API_KEY')
-base64string = base64.encodestring
 
-request = urllib.request.Request(url)
-request.add_header('Authorization', 'Bearer {}'.format(facebook_api_key))
+data, next = return_data(facebook_api_key)
+condition = True
 
-response = urllib.request.urlopen(request)
-encoding = response.headers.get_content_charset()
-# if we don't get anything, assume 'utf-8'
-if encoding is None:
-    print('encoding none!')
-    encoding = 'utf-8'
+while(condition):
+    data, next = return_data(facebook_api_key, next)
+    print(len(data))
 
-data = json.loads(response.read().decode(encoding))
-print(data)
+def return_data(api_key, continuation_url=None):
+    if continuation_url is None:
+        url = base_url + "/me/photos"
+    else:
+        url = base_url + continuation_url
+
+    request = urllib.request.Request(url)
+    request.add_header('Authorization', 'Bearer {}'.format(api_key))
+
+    response = urllib.request.urlopen(request)
+    encoding = response.headers.get_content_charset()
+
+    data = json.loads(response.read().decode(encoding))
+    # NOTE: if you want more parsing, do it here!
+
+    # wrap in try catch?
+    next = data['paging']['next']
+    data = data['data']
+    return data, next
