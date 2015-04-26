ca98d5eef0e3d7ca75a0f753bb6327c07a6184a1
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-04-26 08:22:24 -0500

    successfully got out all fb pictures!

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import json
 import os
 import urllib.request
 
-PIC_NUM = 0
-
 def return_data(url, api_key):
     request = urllib.request.Request(url)
     request.add_header('Authorization', 'Bearer {}'.format(api_key))
@@ -15,24 +13,6 @@ def return_data(url, api_key):
     
     return data 
 
-def parse_images(data, user_id, open_tagged_file):
-    for data_object in data:
-        picture_url = data_object['source']
-
-        f = open('{}.jpg'.format(PIC_NUM), 'wb')
-        f.write(urllib.request.urlopen(picture_url).read())
-        f.close()
-
-        PIC_NUM += 1
-        picture_tags = data_object['tags']
-        # NOTE: Start here
-        #urllib.request.
-        for tagee in picture_tags:
-            if tagee == user_id:
-                # This will give us access to the x, y coordinates of 
-                # the tagged face!
-                pass
-
 def get_next_from_data(data):
     if 'paging' in data:
         paging = data['paging']
@@ -43,24 +23,62 @@ def get_next_from_data(data):
     else:
         return None
 
+def parse_images(data, user_id, picture_number):
+    # TODO: Move into own dir
+    face_coordinate_file = open('face_coordinates.txt', 'a')
+    # Facebook defaults to returning 25 pictures
+    for data_object in data:
+        picture_url = data_object['source']
+        f = open('{}.jpg'.format(picture_number), 'wb')
+        f.write(urllib.request.urlopen(picture_url).read())
+        f.close()
+
+        # NOTE: Assume pictures always have tags!
+        picture_tags = data_object['tags']['data']
+        for data_tag in picture_tags:
+            if data_tag['id'] == user_id:
+                x_face_coordinate = data_tag['x']
+                y_face_coordinate = data_tag['y']
+                face_coordinate_file.write("{},{},{}\n".format(picture_number,
+                                                               x_face_coordinate,
+                                                               y_face_coordinate))
+
+        # Increment picture number for each picture 
+        picture_number = picture_number + 1
+    face_coordinate_file.close()
+
+
 if __name__ == '__main__':
+    # TODO: create face_coordinate file off the get go/ or something
     base_url = "https://graph.facebook.com/v2.3/"
 
     #super secret api password thingy!
     facebook_api_key = os.getenv('FACEBOOK_API_KEY')
 
-    data = return_data(base_url+"me", facebook_api_key)
+    # Make dir for pcitures and change into that directory
+    if not os.path.exists("pictures"):
+        os.makedirs("pictures")
+    os.chdir("pictures")
 
+    # If this script is run multiple times, removes issues with appending coords
+    if os.path.isfile('face_coordinates.txt'):
+        os.remove('face_coordinates.txt')
+    
+    # This is getting out the user id!
+    data = return_data(base_url+"me", facebook_api_key)
     # Will need id for parsing photo tags
     user_id = data['id']
 
+    # This is getting out the first set of pictures!
     data = return_data(base_url+"me/photos", facebook_api_key)
-
     next_ = get_next_from_data(data)
 
     data = data['data']
+    picture_number = 0
+    parse_images(data, user_id, picture_number)
+    picture_number += len(data)
     print(len(data))
-
+    
     if next_ is None:
         more_photos = False
     else:
@@ -71,8 +89,8 @@ if __name__ == '__main__':
         next_ = get_next_from_data(data)
 
         data = data['data']
-
-
+        parse_images(data, user_id, picture_number)
+        picture_number += len(data)
         print(len(data))
         if next_ is None:
             more_photos=False
