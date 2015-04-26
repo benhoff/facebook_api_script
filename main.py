07dfb70460b25b7535b03d96e9430bb52f3d3e9f
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-04-25 21:10:02 -0500

    slight cruft cleaning. Untested

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import urllib.request
 
 PIC_NUM = 0
 
-def return_data(url, api_key, continuation_url=None):
-    if continuation_url is None:
-        url = base_url + "me/photos"
-    else:
-        url = continuation_url
-
+def return_data(url, api_key):
     request = urllib.request.Request(url)
     request.add_header('Authorization', 'Bearer {}'.format(api_key))
 
@@ -18,17 +13,15 @@ def return_data(url, api_key, continuation_url=None):
 
     data = json.loads(response.read().decode(encoding))
     # NOTE: if you want more parsing, do it here!
-
-    # wrap in try catch?
-    print("Data keys: ", data.keys())
+    #print("Data keys: ", data.keys())
     paging = data['paging']
-    print("Paging Keys: ", paging.keys())
+    #print("Paging Keys: ", paging.keys())
     if 'next' in paging:
-        next = paging['next']
+        next_ = paging['next']
     else:
-        next=None
+        next_=None
 
-    return data, next
+    return data, next_
 
 def parse_images(data, user_id, open_tagged_file):
     for data_object in data:
@@ -40,7 +33,8 @@ def parse_images(data, user_id, open_tagged_file):
 
         PIC_NUM += 1
         picture_tags = data_object['tags']
-        urllib.request.
+        # NOTE: Start here
+        #urllib.request.
         for tagee in picture_tags:
             if tagee == user_id:
                 # This will give us access to the x, y coordinates of 
@@ -51,24 +45,24 @@ if __name__ == '__main__':
 
     #super secret api password thingy!
     facebook_api_key = os.getenv('FACEBOOK_API_KEY')
-    data, next = return_data(base_url+"me", facebook_api_key)
+
+    data, next_ = return_data(base_url+"me", facebook_api_key)
 
     # Will need id for parsing photo tags
     user_id = data['id']
 
-    data, next = return_data(base_url+"me/photos", facebook_api_key)
+    data, next_ = return_data(base_url+"me/photos", facebook_api_key)
     data = data['data']
     print(len(data))
     condition = True
 
     while(condition):
-        data, next = return_data(base_url, facebook_api_key, next)
+        data, next_ = return_data(next_, facebook_api_key)
         data = data['data']
 
 
         print(len(data))
-        if next is None:
+        if next_ is None:
             condition=False
 
     print("Done!")
-
