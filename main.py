9cd1ed001495fe5129dd953e58ec803543f8d05a
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-04-26 13:58:58 -0500

    working iterate through files, not face detecting

--- main.py
+++ main.py
@@ -2,6 +2,55 @@ import os
 import csv
 import numpy
 import cv2
+def parse_face_coord_file(face_coord_txt_file=None):
+    result = []
+    if not face_coord_txt_file is None:
+        with open(face_coord_txt_file, 'r') as csvfile:
+            linereader = csv.reader(csvfile, delimiter=',', quotechar='\n')
+            result = []
+            for row in linereader:
+                temp = []
+                for item in row:
+                    try:
+                        temp.append(float(item))
+                    except ValueError:
+                        temp.append(None)
+                result.append(tuple(temp))
+    return result
+
+def display_images(filenames, face_cascade, face_coordinate_list=None):
+    for index, file in enumerate(filenames):
+        # Reads the image out of the file
+        cv_frame = cv2.imread(file)
+        # Turns image from BGR (color scheme) into gray!
+        gray_image = cv2.cvtColor(cv_frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray_image, 1.3, 5)
+        print(faces)
+        
+        for (x, y, w, h) in faces:
+            cv2.rectangle(cv_frame, (x,y),(w+w, y+h), (255, 0, 0), 2)
+        
+        # NOTE: This is the coordinates from facebook!
+        if face_coordinate_list:
+            image_height, image_width, _ = cv_frame.shape 
+
+            # Facebook keeps coords as percentages!
+            if face_coordinate_list:
+                x_and_y_coordinates = face_coordinate_list[index][-2:]
+                if not x_and_y_coordinates[0] is None:
+                    transformed_coords = (int(image_width/100 * x_and_y_coordinates[0]), 
+                                          int(image_height/100 * x_and_y_coordinates[1]))
+
+                    cv2.circle(cv_frame, transformed_coords, 20, (255, 106, 255))
+
+
+        while(True):
+            cv2.imshow('{}'.format(filenames[index]), cv_frame)
+            cv2.imshow('gray', gray_image)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cv2.destroyWindow(filenames[index])
 
 if __name__ == '__main__':
 
@@ -23,53 +72,14 @@ if __name__ == '__main__':
         if len(face_coord_txt_file) < 1:
             face_coord_txt_file = None
     
-    face_coordinate_list = []
-    if not face_coord_txt_file is None:
-        with open(face_coord_txt_file, 'r') as csvfile:
-            linereader = csv.reader(csvfile, delimiter=',', quotechar='\n')
-            for row in linereader:
-                result = []
-                for item in row:
-                    try:
-                        result.append(float(item))
-                    except ValueError:
-                        result.append(None)
-                face_coordinate_list.append(result)
+    face_coordinate_list = parse_face_coord_file(face_coord_txt_file)
+
     if not os.path.exists('../haarcascade_frontalface_default.xml'):
         print("Could not find haarcascade file!")
+
     face_cascade = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml')
-    video_capture = cv2.VideoCapture(filenames[0])
-   
-    # Reads the image out of the file
-    return_value, cv_frame = video_capture.read()
 
-    # Turns image from BGR (color scheme) into gray!
-    gray_image = cv2.cvtColor(cv_frame, cv2.COLOR_BGR2GRAY)
-     
-    faces = face_cascade.detectMultiScale(gray_image, 1.3, 5)
+    # this functionality needs to be looped/changed
+    display_images(filenames, face_cascade, face_coordinate_list)
     
-    for (x, y, w, h) in faces:
-        cv2.rectangle(cv_frame, (x,y),(w+w, y+h), (255, 0, 0), 2)
-    
-    # NOTE: This is the coordinates from facebook!
-    if face_coordinate_list:
-        image_height, image_width, _ = cv_frame.shape 
-
-        # These are percentages!
-        x_and_y_coordinates = face_coordinate_list[0][-2:]
-        transformed_coords = (int(image_width/100 * x_and_y_coordinates[0]), 
-                              int(image_height/100 * x_and_y_coordinates[1]))
-
-        print(x_and_y_coordinates, transformed_coords, image_height, image_width)
-        cv2.circle(cv_frame, transformed_coords, 20, (255, 106, 255))
-        #cv2.rectangle(cv_frame, x_and_y_coordinates,(), (0, 255, 0), 2)
-
-
-    while(True):
-        cv2.imshow('{}'.format(filenames[0]), cv_frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
     cv2.destroyAllWindows()
