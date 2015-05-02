1a526eff4c39d371886b1f814d7102480ab39873
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-05-01 23:29:11 -0500

    nominally added in logic to do LBPH. untested.

--- face_id_from_webcam.cpp
+++ face_id_from_webcam.cpp
@@ -13,9 +13,10 @@ int main(int argc, const char *argv[])
     cv::CascadeClassifier face_classifier;
     bool loaded = face_classifier.load(haarcascade_string);
     cv::VideoCapture video_capture(1);
-    cv::RNG rng(12345);
+    //cv::RNG rng(12345);
 
     cv::Mat frame;
+    // This loops you in the GUI until you're ready to run!
     while(true)
     {
         cv::Mat gray_frame;
@@ -40,6 +41,31 @@ int main(int argc, const char *argv[])
             break;
     }
 
+    std::vector<cv::Mat> images;
+    std::vector<int> labels;
+    cv::Ptr<cv::FaceRecognizer> model = cv::createLBPHFaceRecognizer();
+
+    // Right now defaulting to 20 images.
+    for(size_t i=0; i<20; i++)
+    {
+        labels.push_back(i);
+        cv::Mat gray_frame;
+        video_capture.read(frame);
+        cv::cvtColor(frame, gray_frame, CV_BGR2GRAY);
+
+        face_classifier.detectMultiScale(gray_frame, faces, 1.1, 2,
+                                         0|CV_HAAR_SCALE_IMAGE,
+                                         cv::Size(frame.cols/4, frame.rows/4));
+
+        for(size_t i=0; i<faces.size(); i++)
+        {
+            cv::rectangle(frame, faces[i], cv::Scalar( 255, 0, 255 ));
+            cv::Mat face_region_of_interest = gray_frame(faces[i]);
+            images.push_back(face_region_of_interest);
+        }
+    }
+    model->train(images, labels);
+    model->save('individual_face.xml');
     video_capture.~VideoCapture();
     face_classifier.~CascadeClassifier();
     cv::destroyAllWindows();
