fcc50b4655983e9ec3e21ed8ac49fd244d311606
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-07-27 22:12:26 -0500

    added in haarcascde for eyes

---
+++ eye_aligner.py
@@ -0,0 +1,13 @@
+import os
+import cv2
+
+file_dir = os.path.realpath(__file__) 
+
+cropped_photos_dir = os.path.join(file_dir, 
+                                  'cropped_photos', 
+                                  '')
+
+
+
+
+
